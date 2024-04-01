65430592a3ab4577429b865dc830e8db4d61f254
Muzuwi <kedziora99@gmail.com>
2024-04-02 00:06:38 +0200

    ch3: Checking things with null bytes

--- tools/sendresp.py
+++ tools/sendresp.py
@@ -21,9 +21,19 @@ def main():
     # line += b'\r\n\r\n'
 
     # Fills the entire wRequestData buffer
-    line = b'GET /secret?'
-    line += b'A\r' * ((2048 - 4 - len(line)) // 2)
-    line += b'\r\n\r\n'
+    # line = b'GET /secret?'
+    # line += b'A\r' * ((2048 - 4 - len(line)) // 2)
+    # line += b'\r\n\r\n'
+
+    # Adding the null makes the server not complain about size
+    # thus, the request is only handled until the first null byte
+    # "HTTP" btw
+    line = b'A' * 254
+    line += b"\r\n\0"
+    # 254 -> not mapped to any action
+    # 255 -> request entity too large
+    line += b'B' * 255
+    line += b"\r\n\r\n"
 
     print("Sending line:", line, "of length:", len(line))
 
