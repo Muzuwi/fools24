fe8e3a9b7e9aeed0380753b4126a37cf37ba098f
Muzuwi <kedziora99@gmail.com>
2024-04-07 15:44:59 +0200

    missingno: Broken differential decoding

---
+++ tools/missingno/differential.py
@@ -0,0 +1,76 @@
+
+
+class DifferentialDecoder():
+    DECODE_NYBBLE0_TABLE = [ 0x01,0x32,0x76,0x45,0xfe,0xcd,0x89,0xba ]
+    DECODE_NYBBLE1_TABLE = [ 0xfe,0xcd,0x89,0xba,0x01,0x32,0x76,0x45 ]
+    DECODE_NYBBLE0_FLIPPED_TABLE = [ 0x08,0xc4,0xe6,0x2a,0xf7,0x3b,0x19,0xd5 ]
+    DECODE_NYBBLE1_FLIPPED_TABLE = [ 0xf7,0x3b,0x19,0xd5,0x08,0xc4,0xe6,0x2a ]
+
+
+    def __init__(self,
+                 input: bytes,
+                 width: int,
+                 height: int,
+                 table0 = DECODE_NYBBLE0_TABLE,
+                 table1 = DECODE_NYBBLE1_TABLE,
+                 invert = False) -> None:
+        self.input = input
+        self.width = width
+        self.height = height
+        self.table_0 = table0
+        self.table_1 = table1
+        self.invert = invert
+        self.previous_data = 1 if invert else 0
+
+
+    def decode_nybble(self, nybble: int) -> int:
+        if self.invert:
+            initial = self.previous_data & 0b1000
+        else:
+            initial = self.previous_data & 0b0001
+
+        if not initial:
+            table = self.table_0
+        else:
+            table = self.table_1
+
+        byte = table[nybble >> 1]
+        if byte & 0b1:
+            new_nybble = (byte >> 4) & 0xF
+        else:
+            new_nybble = byte & 0xF
+
+        self.previous_data = new_nybble
+        return self.previous_data
+
+
+    def decode(self) -> bytearray:
+        output = bytearray(b'\x00' * len(self.input))
+
+        ptr = 0
+        ptr_cached = 0
+        x = 0
+        y = 0
+        while True:
+            byte = self.input[ptr]
+            high = (byte >> 4) & 0xF
+            low = byte & 0xF
+
+            high = self.decode_nybble(high)
+            low = self.decode_nybble(low)
+
+            output[ptr] = (high << 4) | low
+            ptr += self.height
+
+            x += 8
+            if x >= self.width:
+                x = 0
+                y += 1
+                if y >= self.height:
+                    y = 0
+                    break
+
+                ptr_cached += 1
+                ptr = ptr_cached
+
+        return output
